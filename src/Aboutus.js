0103ff9888ee47a9256f7886baaa22849461a62a
Josh Jestine <joshjustin007@gmail.com>
2022-10-13 20:58:12 +0530

    Upload

---
+++ src/Aboutus.js
@@ -0,0 +1,3 @@
+export function Aboutus(){
+    return <h1>Aboutus</h1>;
+}
--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+//Josh Jestine, Roll Number : 3
+import {Routes, Route, Link} from "react-router-dom";
+import {Home} from './home'
+import {Contact} from './Contact'
+import {Aboutus} from './Aboutus'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/Contact">Contact</Link>
+          </li>
+          <li>
+            <Link to="/Aboutus">About us</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path = "/" element = {<Home />}></Route>
+        <Route path = "/Contact" element = {<Contact />}></Route>
+        <Route path = "/Aboutus" element = {<Aboutus />}></Route>
+        </Routes>
     </div>
   );
 }
